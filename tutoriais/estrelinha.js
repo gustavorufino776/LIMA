db22a8b08d4b5532fb56c0d70ee49c410f516d56
gustavorufino776 <gustavorufino776@gmail.com>
2026-08-07 16:46:33 -0300

    Criando um site

---
+++ tutoriais/estrelinha.js
@@ -0,0 +1,22 @@
+/*Adicionando um "Listener" para o movimento do mouse*/
+window.addEventListener("mousemove" , (elemento) =>{
+    /*Criando uma div contendo o caracter que representa uma estrela*/
+    const estrelinha = document.createElement("div");
+    estrelinha.className = "estrelinha";
+    estrelinha.innerHTML = "&#10022";
+    estrelinha.style.left = elemento.clientX + "px";
+    estrelinha.style.top = elemento.clientY + "px";
+    
+    //Criando uma deriva horizontal para o efeito de queda da estrela
+    const xAleatorio = (Math.random() - 0.5) * 50 + "px";
+    estrelinha.style.setProperty("--xAleatorio", xAleatorio);
+    
+    /*Acrecentando a div classe "estrelinha" ao body*/
+    document.body.appendChild(estrelinha);
+
+    //removendo o elemento após o término da animação (800ms)
+    setTimeout(()=>{
+        estrelinha.remove();
+    }, 800)
+
+})
